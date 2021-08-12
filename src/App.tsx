39c0baf0f107e80a6d4f983485806309e58cf755
Malaz Alkoj <malaz.alkoj@nav.no>
2021-08-12 11:49:32 +0200

    legge til komponentkode

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Samtalestøttepanel from "./Samtalestøttepanel/Samtalestøttepanel";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          starter
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         TeamIA
-        </a>
-      </header>
+      <Samtalestøttepanel/>
     </div>
   );
 }
---
+++ src/konstanter.ts
@@ -0,0 +1,14 @@
+export const BASE_PATH = '/sykefravarsstatistikk';
+export const FRONTEND_API_PATH = BASE_PATH + '/api';
+
+export const PATH_FORSIDE = '/';
+export const PATH_FORSIDE_GENERELL = '/sammenligning';
+export const PATH_FORSIDE_BARNEHAGE = PATH_FORSIDE_GENERELL + '/barnehage';
+export const PATH_KALKULATOR = '/kalkulator';
+export const PATH_HISTORIKK = '/historikk';
+export const PATH_IAWEB_REDIRECTSIDE = '/iawebredirectside';
+export const PATH_SAMTALESTOTTE_PODLET = '/samtalestotte-podlet';
+
+export const PATH_SAMTALESTØTTE = '/samtalestotte';
+
+export const ER_VEDLIKEHOLD_AKTIVERT = false;
