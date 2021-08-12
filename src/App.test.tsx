3a26517069b665adbe94deebfe479ba6ee7968b0
Malaz Alkoj <malaz.alkoj@nav.no>
2021-08-12 13:16:38 +0200

    fikse tester

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders tittel til podlet', () => {
   render(<App />);
-  const linkElement = screen.getByText(/TeamIA/i);
+  const linkElement = screen.getByText(/Forbered samtale med medarbeider!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('Lenke tekst rendres som forventet', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Gå til samtalestøtten/i);
   expect(linkElement).toBeInTheDocument();
 });
