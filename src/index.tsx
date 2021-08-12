dde409bf8409902562f197a302fb1630d71da835
Malaz Alkoj <malaz.alkoj@nav.no>
2021-08-12 16:05:37 +0200

    eksportere applikasjon med navspa

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {FunctionComponent} from 'react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navspa from '@navikt/navspa';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const AppContainer: FunctionComponent = () => (
+    <div id="rekrutteringsbistand-kandidat" >
+                    <App />
+    </div>
+);
+Navspa.eksporter('samtalestotte-podlet', AppContainer);
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
