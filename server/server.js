fbb5cd1db268c5a98b9228686f0db3b6a542beb1
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-08-17 13:30:17 +0200

    Forenkle server.js (fjerne ubrukte env variabler)

--- server/server.js
+++ server/server.js
@@ -2,31 +2,16 @@
 const path = require('path');
 const express = require('express');
 
-const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3001;
 
 const basePath = '/samtalestotte-podlet';
 const buildPath = path.join(__dirname, '../build');
-const envPath = 'static/js/env.js';
 
-const manifestMedEnvpath = () => {
-    const asset = JSON.parse(fs.readFileSync(`${buildPath}/asset-manifest.json`, 'utf8'));
-    if (asset.files) {
-        const name = envPath.split('/').pop();
-        asset.files[name] = `${basePath}/${envPath}`;
-    }
-    return JSON.stringify(asset, null, 4);
-};
-
-const manifest = manifestMedEnvpath();
 
 const startServer = () => {
     app.use(`${basePath}/static`, express.static(buildPath + '/static'));
-
-    app.get(`${basePath}/asset-manifest.json`, (req, res) => {
-        res.type('json').send(manifest);
-    });
+    app.use(`${basePath}/asset-manifest.json`, express.static(buildPath + '/asset-manifest.json'));
 
     app.get([`${basePath}/internal/isAlive`, `${basePath}/internal/isReady`], (req, res) =>
         res.sendStatus(200)
