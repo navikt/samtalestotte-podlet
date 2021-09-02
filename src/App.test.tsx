f892aa10b217858c8eec6d1cbad923ee193a6a77
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-09-02 12:41:24 +0200

    Fiks: test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders tittel til podlet', () => {
-    render(<App prodDomener={['localhost']} visning={'SNAKKEBOBLE'} />);
+    render(<App prodDomener={['localhost']} visning={'PANEL_MED_IKON_OG_TEKST'} />);
     const linkElement = screen.getByText(/Forbered samtale med medarbeider!/i);
     expect(linkElement).toBeInTheDocument();
 });
