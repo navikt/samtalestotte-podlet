8df94152f2afef58755c13c67b638df2c8290d62
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-09-02 07:53:54 +0200

    Andre design tilgjengelig: snakkeboble

--- src/Samtalestøttepanel/Samtalestøttepanel.tsx
+++ src/Samtalestøttepanel/Samtalestøttepanel.tsx
@@ -1,12 +1,14 @@
 import React, {FunctionComponent} from 'react';
 import {Normaltekst} from 'nav-frontend-typografi';
 import {ReactComponent as Lampesvg} from './lampe.svg';
+import female from './female.svg';
 import './Samtalestøttepanel.less';
 import {PaneltittelMedIkon} from '../PaneltittelMedIkon/PaneltittelMedIkon';
 import {PATH_SAMTALESTØTTE} from '../konstanter';
 import classNames from 'classnames';
 import Lenke from 'nav-frontend-lenker';
 import '../InternLenke/InternLenke.less';
+import {Snakkeboble} from "../Snakkeboble/Snakkeboble";
 
 import {Visningsmodus} from "../App";
 
@@ -17,9 +19,20 @@ type SamtalestøttePanelProps = {
 const Samtalestøttepanel: FunctionComponent<SamtalestøttePanelProps> = ({visningsmodus}) => {
     const lenkeTekst = 'Gå til samtalestøtten';
 
+    const fellesLenke =<Lenke
+        href={PATH_SAMTALESTØTTE + '?referer=' + window.location.href}
+        className={classNames('intern-lenke')}
+    >
+        {lenkeTekst}
+    </Lenke>;
+
     if (visningsmodus === Visningsmodus.SNAKKEBOBLE) {
-        return (<></>);
-    } else {
+        return (<>
+            <Snakkeboble src={female} alt="Female rådgiver">
+                {<Normaltekst>Samtaler rundt sykefravær kan være vanskelige. Vi har laget et verktøy for arbeidsgivere for å gjøre det lettere å forberede seg til samtaler med medarbeidere!
+                </Normaltekst>}
+            </Snakkeboble>
+            {fellesLenke}</>);    } else {
         return (
             <>
                 <PaneltittelMedIkon src={<Lampesvg/>} alt="lampeikon">
@@ -29,12 +42,7 @@ const Samtalestøttepanel: FunctionComponent<SamtalestøttePanelProps> = ({visni
                     Samtaler rundt sykefravær kan være vanskelige. Vi har laget et verktøy for
                     arbeidsgivere for å gjøre det lettere å forberede seg.
                 </Normaltekst>
-                <Lenke
-                    href={PATH_SAMTALESTØTTE + '?referer=' + window.location.href}
-                    className={classNames('intern-lenke')}
-                >
-                    {lenkeTekst}
-                </Lenke>
+                {fellesLenke}
             </>)
     }
 };
---
+++ src/Snakkeboble/Snakkeboble.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react';
+import { Systemtittel } from 'nav-frontend-typografi';
+import "./Snakkeboble.less";
+
+interface Props {
+    src: any;
+    alt: string;
+}
+
+export const Snakkeboble: FunctionComponent<Props> = (props) => (
+    <div className="paneltittel-med-ikon">
+        <img className="paneltittel-med-ikon__ikon" src={props.src} alt={props.alt} />
+        <Systemtittel className="paneltittel-med-ikon__tittel" tag="h2">
+            {props.children}
+        </Systemtittel>
+    </div>
+);
