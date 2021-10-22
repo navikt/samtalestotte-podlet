271c4ac67813e24cea5b4c9ed3704c7c2392e33f
Malaz Alkoj <malaz.alkoj@nav.no>
2021-10-22 13:30:27 +0200

    velge tilpasset preprod lenke ut i fra nåværende dev-miljø

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import {
     DEV_GCP_DOMENE_ARBEIDSGIVER,
+    LABS_DOMENE_ARBEIDSGIVER,
     PATH_SAMTALESTØTTE,
     PROD_DOMENE_ARBEIDSGIVER,
 } from './konstanter';
@@ -24,13 +25,15 @@ export function erProdMiljø(prodDomener: string[], origin: string) {
     });
     return kjørerIProd;
 }
-
+const velgPreprodDomene = (origin: string): string => {
+    return origin.includes('-q.nav.no') ? DEV_GCP_DOMENE_ARBEIDSGIVER : LABS_DOMENE_ARBEIDSGIVER;
+};
 export const getSamtalestøtteUrl = (domener: string[]): string => {
     const origin = window.location.origin;
 
     if (erProdMiljø(domener, origin)) {
         return `https://${PROD_DOMENE_ARBEIDSGIVER}${PATH_SAMTALESTØTTE}`;
     } else {
-        return `https://${DEV_GCP_DOMENE_ARBEIDSGIVER}${PATH_SAMTALESTØTTE}`;
+        return `https://${velgPreprodDomene(origin)}${PATH_SAMTALESTØTTE}`;
     }
 };
